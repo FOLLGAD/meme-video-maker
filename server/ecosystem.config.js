7e0e131822c8d6ea2ff84aa97ebf4364dc53f372
Emil Ahlbäck <emil@tentium.se>
2020-10-05 18:44:56 +0200

    updated ecosystem

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,18 +1,24 @@
 module.exports = {
-	apps: [],
+    apps: [
+        {
+            name: "carp",
+            script: "dist/server.js",
+            watch: "dist",
+            post_update: ["npm i", "npm run build"],
+        },
+    ],
 
-	deploy: {
-		production: {
-			key: "~/.ssh/redditors-expansion.pem",
-			user: "ec2-user",
-			host: "image.redditvideomaker.com",
-			ref: "origin/master",
-			repo: "git@github.com:FOLLGAD/4chan-reader.git",
-			path: "/home/ec2-user/4chan",
-			"pre-deploy-local": "",
-			"post-deploy":
-				"npm install && npm run build && cd ../.. && pm2 reload ecosystem.config.js --env production",
-			"pre-setup": "",
-		},
-	},
+    deploy: {
+        production: {
+            key: "~/.ssh/redditors-expansion.pem",
+            user: "ubuntu",
+            host: "image.redditvideomaker.com",
+            ref: "origin/master",
+            repo: "git@github.com:FOLLGAD/carp.git",
+            path: "~/carp/server",
+            "pre-deploy-local": "",
+            "post-deploy": ["npm i", "npm run build"],
+            "pre-setup": "",
+        },
+    },
 }
