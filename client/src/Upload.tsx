3c80780cbf2f67119ef2a121921d02b081ade713
Emil Ahlbäck <emil@tentium.se>
2020-10-05 17:40:08 +0200

    hotfix ts frontend

--- client/src/Upload.tsx
+++ client/src/Upload.tsx
@@ -1,92 +1,54 @@
 import React, { useRef, useState } from "react"
-import apiFetch from "./apiFetch"
 import { v4 as uuidv4 } from "uuid"
-/// <reference types="aws-sdk" />
-import * as AWS from "aws-sdk"
-import { CognitoIdentityCredentials, Credentials } from "aws-sdk"
-
-// `AWS.region = "..."` disables autocomplete in VS Code for some reason!! wtf
-
-// AWS.region = "eu-central-1" // Region
-// AWS.credentials = new AWS.CognitoIdentityCredentials({
-//     IdentityPoolId: "eu-central-1:f9d40dab-3659-4f21-8e65-dbf590212d7b",
-// })
-const s3 = new AWS.S3({
-    region: "eu-central-1",
-    credentials: new CognitoIdentityCredentials({
-        IdentityPoolId: "f9d40dab-3659-4f21-8e65-dbf590212d7b",
-    }),
-})
-
-const MemesBucket = "carp-memes"
+import apiFetch from "./apiFetch"
 
 export default function Form({ setData }: { setData: (data: any) => void }) {
     const filesInp = useRef<HTMLInputElement>(null)
     const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(false)
+    const [error, setError] = useState<string | null>(null)
 
     const submit = async (
         event: React.MouseEvent<HTMLButtonElement, MouseEvent>
     ) => {
         event.preventDefault()
 
-        if (!filesInp.current || !filesInp.current.files) return
-
-        const { files } = filesInp.current
-        if (files.length === 0) {
+        const fd = new FormData()
+        const files = filesInp!.current!.files
+        if (!files || files.length === 0) {
             return
         }
+        setLoading(true)
+        setError(null)
+
+        // Append files to formdata
+        const info: { id: string }[] = []
         for (let i = 0; i < files.length; i++) {
             const file = files[i]
-            /*
-                TODO:
-                - add the cognito pool id somehow
-                - upload all images there
-                - send the urls to the carp API instead of the images
-                - change the api to accept urls for google vision
-                - same as above but for the video rendering part
-            */
-            const origname = file.name
-            const key = uuidv4()
-            const val = await new Promise<AWS.S3.PutObjectOutput>(
-                (res, rej) => {
-                    s3.putObject(
-                        {
-                            Bucket: MemesBucket,
-                            Key: key,
-                            Body: file,
-                            Metadata: {
-                                filename: origname,
-                            },
-                        },
-                        (err, data) => {
-                            err ? rej(err) : res(data)
-                        }
-                    )
-                }
-            )
-            console.log("Etag:", val.ETag)
+            fd.append("files", file, i.toString())
+            info.push({ id: i.toString() })
         }
 
-        // apiFetch("/vision", {
-        //     // body: fd,
-        //     method: "POST",
-        // })
-        //     .then(async (d) => {
-        //         if (!d.ok) {
-        //             throw await d.json()
-        //         }
-        //         return await d.json()
-        //     })
-        //     .then((d) => {
-        //         setLoading(false)
-        //         setData({ res: d, images: files })
-        //     })
-        //     .catch((d) => {
-        //         setLoading(false)
-        //         console.log(d)
-        //         setError(d.message || d.error)
-        //     })
+        fd.append("info", JSON.stringify(info))
+
+        apiFetch("/vision", {
+            body: fd,
+            method: "POST",
+        })
+            .then(async (d) => {
+                if (!d.ok) {
+                    throw await d.json()
+                }
+                return await d.json()
+            })
+            .then((d) => {
+                setLoading(false)
+                setData({ res: d, images: files })
+            })
+            .catch((d) => {
+                setLoading(false)
+                console.log(d)
+                setError(d.message || d.error)
+            })
     }
 
     return (
