f92c5588e9a4a646fb1d9cd49388145f99b8dd19
Samuel Söderberg <samuelsoderberg99@gmail.com>
2020-10-07 13:49:54 +0200

    added possible break-inducing words

--- server/src/string-formatter.ts
+++ server/src/string-formatter.ts
@@ -9,6 +9,64 @@ let teststring3 = ["Me and my boys just chilling!"]
 
 let allStrings = [testString, teststring2, teststring3]
 
+let pausers = [
+    "or",
+    "but",
+    "so",
+    "after",
+    "before",
+    "even if",
+    "if",
+    "just as",
+    "provided",
+    "since",
+    "that",
+    "until",
+    "whenever",
+    "whereas",
+    "wherever",
+    "althought",
+    "as if",
+    "because",
+    "even though",
+    "if only",
+    "if then",
+    "lest",
+    "when",
+    "provided",
+    "rather than",
+    "so that",
+]
+
+let pausers2 = [
+    "or",
+    "but",
+    "so",
+    "after",
+    "before",
+    "even",
+    "if",
+    "just",
+    "provided",
+    "since",
+    "that",
+    "until",
+    "whenever",
+    "whereas",
+    "wherever",
+    "althought",
+    "as",
+    "because",
+    "even though",
+    "only",
+    "then",
+    "lest",
+    "when",
+    "provided",
+    "than",
+    "rather",
+]
+
 // input is array of strings
 function stringFormatter(input: string[]) {
     return input.map((line) => addDashes(line))
@@ -18,10 +76,10 @@ function stringFormatter(input: string[]) {
 function addDashes(line: string) {
     let lineRes = ""
     line.split(" ").forEach((word, i) => {
-        if (word == "why" && i >= 2) lineRes += "-" + word
-        else if (word == "and" && i >= 2)
-            lineRes +=
-                ' <phoneme alphabet="ipa" ph="ænd">thisisnotawort</phoneme>'
+        if (pausers2.includes(word) && i >= 2) lineRes += "-" + word
+        // else if (word == "and" && i >= 2)
+        //     lineRes +=
+        //         ' <phoneme alphabet="ipa" ph="ænd">thisisnotawort</phoneme>'
         else lineRes += " " + word
     })
     return lineRes.substring(1)
